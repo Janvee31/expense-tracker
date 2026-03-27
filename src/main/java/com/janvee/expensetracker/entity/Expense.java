5df9d04e7efaa9a2c0da83df93bc2a709f5114a2
Janvee <janvee0231@gmail.com>
2026-03-27 16:56:12 +0530

    Day 6: expenses?page=0&size=5&sortBy=amount

--- src/main/java/com/janvee/expensetracker/entity/Expense.java
+++ src/main/java/com/janvee/expensetracker/entity/Expense.java
@@ -3,56 +3,42 @@ package com.janvee.expensetracker.entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "expenses")
 public class Expense {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank
     private String title;
-
     @Positive
     private double amount;
-
     @NotBlank
     private String category;
-
     public Expense() {}
-
     public Expense(String title, double amount, String category) {
         this.title = title;
         this.amount = amount;
         this.category = category;
     }
-
     public Long getId() {
         return id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public double getAmount() {
         return amount;
     }
-
     public String getCategory() {
         return category;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public void setAmount(double amount) {
         this.amount = amount;
     }
-
     public void setCategory(String category) {
         this.category = category;
     }
--- src/main/java/com/janvee/expensetracker/service/ExpenseServiceImpl.java
+++ src/main/java/com/janvee/expensetracker/service/ExpenseServiceImpl.java
@@ -1,63 +1,55 @@
 package com.janvee.expensetracker.service;
 
-import java.util.List;
-
-import com.janvee.expensetracker.exception.ExpenseNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.janvee.expensetracker.entity.Expense;
+import com.janvee.expensetracker.exception.ExpenseNotFoundException;
 import com.janvee.expensetracker.repository.ExpenseRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import java.util.List;
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
-
     @Autowired
     private ExpenseRepository expenseRepository;
-
     @Override
     public Expense saveExpense(Expense expense) {
         return expenseRepository.save(expense);
     }
-
-    @Override
-    public List<Expense> getAllExpenses() {
-        return expenseRepository.findAll();
-    }
-
     @Override
     public Expense getExpenseById(Long id) {
-
         return expenseRepository.findById(id)
-                .orElseThrow(() -> new ExpenseNotFoundException("Expense not found with id " + id));
+                .orElseThrow(() ->
+                        new ExpenseNotFoundException("Expense not found with id " + id));
     }
     @Override
     public void deleteExpense(Long id) {
         expenseRepository.deleteById(id);
     }
+    @Override
     public Expense updateExpense(Long id, Expense expense) {
-        Expense existingExpense = expenseRepository.findById(id).orElse(null);
-        if(existingExpense != null) {
-            existingExpense.setCategory(expense.getCategory());
-            existingExpense.setAmount(expense.getAmount());
-            return expenseRepository.save(existingExpense);
-        }
-        return null;
+        Expense existingExpense = expenseRepository.findById(id)
+                .orElseThrow(() ->
+                        new ExpenseNotFoundException("Expense not found with id " + id));
+        existingExpense.setCategory(expense.getCategory());
+        existingExpense.setAmount(expense.getAmount());
+        return expenseRepository.save(existingExpense);
     }
+    @Override
     public List<Expense> getExpensesByCategory(String category) {
         return expenseRepository.findByCategory(category);
     }
     @Override
     public double getTotalExpenses() {
-
         List<Expense> expenses = expenseRepository.findAll();
-
         double total = 0;
-
         for (Expense expense : expenses) {
             total += expense.getAmount();
         }
-
         return total;
+    }
+    @Override
+    public Page<Expense> getAllExpenses(Pageable pageable) {
+        return expenseRepository.findAll(pageable);
     }
 }
