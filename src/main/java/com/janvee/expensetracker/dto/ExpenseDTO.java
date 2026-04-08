d42507c1ed1a70aee1cf7b6f7c0df5278de923a8
Janvee <janvee0231@gmail.com>
2026-04-08 13:45:45 +0530

    Added update feature

--- src/main/java/com/janvee/expensetracker/dto/ExpenseDTO.java
+++ src/main/java/com/janvee/expensetracker/dto/ExpenseDTO.java
@@ -1,6 +1,7 @@
 package com.janvee.expensetracker.dto;
 
 import lombok.Data;
+import java.time.LocalDate;
 
 @Data
 public class ExpenseDTO {
@@ -8,5 +9,5 @@ public class ExpenseDTO {
     private Long id;
     private String category;
     private double amount;
-
+    private LocalDate date;
 }
